09cb6974030914b927ea3873d50ffb21ce54de3e
Junior Baez <junior_baez@ucne.edu.do>
2022-07-07 18:24:27 -0400

    Modificando las paletas de colores

--- app/src/main/java/com/call/prestamoamigo/ui/theme/Theme.kt
+++ app/src/main/java/com/call/prestamoamigo/ui/theme/Theme.kt
@@ -7,15 +7,21 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = primaryColor,
+    primaryVariant = primaryDarkColor,
+    secondary = secondaryColor,
+    secondaryVariant = secondaryDarkColor,
+    background = backgroundColor,
+    surface = secondaryColor
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = primaryColor,
+    primaryVariant = primaryLightColor,
+    secondary = secondaryColor,
+    secondaryVariant = secondaryLightColor,
+    background = backgroundColor,
+    surface = secondaryColor
 
     /* Other default colors to override
     background = Color.White,
