33c65a1c7163047bfec0b8ea827f9d4e916e4852
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2022-07-07 22:54:09 -0400

    Creando Pagos Db

---
+++ app/src/main/java/com/call/prestamoamigo/data/PagosDb.kt
@@ -0,0 +1,15 @@
+package com.call.prestamoamigo.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.call.prestamoamigo.model.Pago
+import com.call.prestamoamigo.model.Persona
+
+@Database(
+    entities = [Pago::class],
+    version = 1
+)
+abstract class PagosDb: RoomDatabase() {
+    abstract  val pagosDao: PagosDao
+
+}
