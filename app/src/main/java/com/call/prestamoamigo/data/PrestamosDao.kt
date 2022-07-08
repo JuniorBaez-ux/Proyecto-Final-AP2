9a75e70d592614e0974788b75ed7539415789512
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2022-07-07 22:56:40 -0400

    Creando Prestamos Dao

---
+++ app/src/main/java/com/call/prestamoamigo/data/PrestamosDao.kt
@@ -0,0 +1,36 @@
+package com.call.prestamoamigo.data
+
+import androidx.room.*
+import com.call.prestamoamigo.model.Prestamo
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface PrestamosDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun Insertar(prestamo: Prestamo)
+
+    @Delete
+    suspend fun Eliminar(prestamo: Prestamo)
+
+
+    @Query(
+        """
+        SELECT * 
+        FROM Prestamos
+        WHERE prestamoId=:prestamoId        
+    """
+    )
+    fun Buscar(prestamoId: Int): Flow<Prestamo>
+
+    @Query(
+        """
+        SELECT * 
+        FROM Prestamos
+        ORDER BY prestamoId    
+    """
+    )
+    fun GetLista(): Flow<List<Prestamo>>
+
+}
