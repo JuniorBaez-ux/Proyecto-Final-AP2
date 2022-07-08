652602d2cfed91542645c5ccded96ea4c5143348
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2022-07-07 22:56:17 -0400

    Creando Persona Repository

---
+++ app/src/main/java/com/call/prestamoamigo/data/repository/PersonasRepository.kt
@@ -0,0 +1,21 @@
+package com.call.prestamoamigo.data.repository
+
+import com.call.prestamoamigo.data.PersonasDao
+import com.call.prestamoamigo.data.PrestamosDao
+import com.call.prestamoamigo.model.Persona
+import com.call.prestamoamigo.model.Prestamo
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+
+class PersonasRepository @Inject constructor(
+    val personasDao: PersonasDao
+) {
+    suspend fun Insertar(personas: Persona)= personasDao.Insertar(personas)
+
+    fun Buscar(personas: Int): Flow<Persona>   =  personasDao.Buscar(personas)
+
+    suspend fun Eliminar(personas: Persona)= personasDao.Eliminar(personas)
+
+    fun GetLista(): Flow<List<Persona>> = personasDao.GetLista()
+}
