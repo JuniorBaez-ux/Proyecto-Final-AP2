ea3ddfb47d479c20167acb0c62204de33bbfa033
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-07-13 15:49:30 -0400

    Implemetando validaciones

--- app/src/main/java/com/call/prestamoamigo/view/Prestamo/RegistroPrestamoScreen.kt
+++ app/src/main/java/com/call/prestamoamigo/view/Prestamo/RegistroPrestamoScreen.kt
@@ -2,6 +2,7 @@ package com.call.prestamoamigo.ui.components.prestamo
 
 import android.app.DatePickerDialog
 import android.view.View
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,7 +11,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.twotone.Save
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.SemanticsActions.OnClick
@@ -29,17 +36,46 @@ fun RegistroPrestamoSceen(navHostController: NavHostController,
 ){
     val context = LocalContext.current
 
+    var erro by rememberSaveable { mutableStateOf(false)}
+    val Monto = FocusRequester()
+    val Concepto = FocusRequester()
+    val Vence = FocusRequester()
+    val Fecha = FocusRequester()
+
     Scaffold(
         topBar = { TopAppBar(title = { Text(text = "Registro de Prestamo") }) },
             floatingActionButton = {
                  FloatingActionButton(
                       onClick = {
+                          if (PrestamoViewModel.fecha.isNullOrEmpty()) {
+
+                              Toast.makeText(context, "Fecha no puede estar vacio!", Toast.LENGTH_LONG).show()
+                              Fecha.requestFocus()
+                              return@FloatingActionButton
+                          }
+                          if (PrestamoViewModel.vence.isNullOrEmpty()) {
+
+                              Toast.makeText(context, "La fecha Vence está vacio!", Toast.LENGTH_LONG).show()
+                              Vence.requestFocus()
+                              return@FloatingActionButton
+                          }
+                          if (PrestamoViewModel.concepto.isNullOrEmpty()) {
+                              Toast.makeText(context, "El campo Concepto está vacio", Toast.LENGTH_LONG).show()
+                              Concepto.requestFocus()
+                              return@FloatingActionButton
+                          }
+                          if (PrestamoViewModel.monto.toFloat() <= 0) {
+                              Toast.makeText(context, "El Monto debe ser mayor que cero!", Toast.LENGTH_LONG)
+                                  .show()
+                              Monto.requestFocus()
+                              return@FloatingActionButton
+                          }
                           PrestamoViewModel.Guardar()
                     navHostController.navigate("RegistroPrestamo")
                 },
                 backgroundColor = MaterialTheme.colors.background
             ){
-                Icon(imageVector = Icons.TwoTone.Save, contentDescription = null)}
+                Icon(imageVector = Icons.TwoTone.Save, contentDescription = "Save")}
 },
     ) {
         it
@@ -63,7 +99,8 @@ fun RegistroPrestamoSceen(navHostController: NavHostController,
             OutlinedTextField(
                 value = PrestamoViewModel.concepto,
                 onValueChange = { PrestamoViewModel.concepto = it },
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth()
+                    .focusRequester(Concepto),
                 label = {
                     Text(text = "Concepto")
                 },
@@ -78,7 +115,8 @@ fun RegistroPrestamoSceen(navHostController: NavHostController,
             OutlinedTextField(
                 value = PrestamoViewModel.monto,
                 onValueChange = { PrestamoViewModel.monto = it },
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth()
+                    .focusRequester(Monto),
                 label = {
                     Text(text = "Monto")
                 },
@@ -93,7 +131,8 @@ fun RegistroPrestamoSceen(navHostController: NavHostController,
             OutlinedTextField(
                 value = PrestamoViewModel.vence,
                 onValueChange = { PrestamoViewModel.vence = it },
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth()
+                    .focusRequester(Vence),
                 label = {
                     Text(text = "Vence")
                 },
