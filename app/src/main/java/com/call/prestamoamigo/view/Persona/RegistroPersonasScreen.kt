8069611b981c3e92084439e2f16b8dbfa7290545
Junior Baez <junior_baez@ucne.edu.do>
2022-07-15 17:47:50 -0400

    Mejorando diseño del registro de personas

--- app/src/main/java/com/call/prestamoamigo/view/Persona/RegistroPersonasScreen.kt
+++ app/src/main/java/com/call/prestamoamigo/view/Persona/RegistroPersonasScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -102,7 +103,7 @@ fun RegistroPersonasScreen(
 
             }, modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
         ) {
-            Text(text = "Guardar")
+            Text(text = "Guardar", color = Color.Black)
         }
     }
     }
