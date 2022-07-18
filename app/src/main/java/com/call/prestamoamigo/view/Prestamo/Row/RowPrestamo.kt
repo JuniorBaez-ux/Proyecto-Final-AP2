9b5bc9e322d4c3a13b7c595f3f3f2ecb56768695
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-07-18 18:39:25 -0400

    Implementando diseño de consulta de prestamos

---
+++ app/src/main/java/com/call/prestamoamigo/view/Prestamo/Row/RowPrestamo.kt
@@ -0,0 +1,70 @@
+package com.call.prestamoamigo.view.Prestamo.Row
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.call.prestamoamigo.model.Prestamo
+
+
+@Composable
+fun RowPrestamo (prestamo: Prestamo, navHostController: NavHostController) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(6.dp)
+        .background(color = Color(0xFF82D4BB)))
+        {
+        Card(modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clickable {
+                navHostController.navigate("RegistroPrestamo")
+            }){
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(30.dp)
+                    .padding(8.dp),
+
+                ) {
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth().padding(2.dp)
+
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ){
+                    Text(
+                        text = "${prestamo.concepto}",
+                    )
+                        Text("$${prestamo.monto}")
+                }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+
+                        ) {
+                        Text("${prestamo.fecha}",)
+                        Text("$ ${prestamo.balance}",)
+                    }
+                }
+            }
+        }
+    }
+}
