b5a2d9acfe52b183a31b75891b228569d2de31a2
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-07-18 18:36:21 -0400

    Implemetando Diseño de la consulta de prestamos

--- app/src/main/java/com/call/prestamoamigo/view/Prestamo/ConsultaPrestamoScreen.kt
+++ app/src/main/java/com/call/prestamoamigo/view/Prestamo/ConsultaPrestamoScreen.kt
@@ -1,16 +1,22 @@
 package com.call.prestamoamigo.view.Prestamo
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import android.graphics.Color
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.text.font.FontWeight.Companion.Black
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.call.prestamoamigo.view.Pago.pagoItem
+import com.call.prestamoamigo.view.Prestamo.Row.RowPrestamo
 
 
 @Composable
@@ -18,7 +24,7 @@ fun ConsultaPrestamoScreen(
     navHostController: NavHostController,
     PrestamoViewModel: PrestamoViewModel = hiltViewModel()
 ){
-
+    val listaPrestamo = PrestamoViewModel.prestamos.collectAsState(initial = emptyList())
     val ScaffoldState = rememberScaffoldState()
 
     Scaffold(
@@ -28,19 +34,33 @@ fun ConsultaPrestamoScreen(
         },
         floatingActionButton = {
             FloatingActionButton(onClick = {
-                navHostController.navigate("RegistroPrestamos")
+                navHostController.navigate("RegistroPrestamo")
             }) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = null)
             }
         },
         scaffoldState = ScaffoldState
 
-    ) {it
+    ) {
+        it
 
-        Column(modifier = Modifier.padding(8.dp)
-            .fillMaxSize()) {
-
+        Column(
+            modifier = Modifier.padding(8.dp)
+                .fillMaxSize()
+        ) {
+            LazyColumn(modifier = Modifier.fillMaxSize()){
+                items(listaPrestamo.value){ prestamo ->
+                    RowPrestamo(prestamo = prestamo, navHostController = navHostController)
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Divider(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .width(1.dp)
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
             }
         }
+    }
 }
 
