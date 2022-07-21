da16889c0600ffa47634343de7131f76eec9a02d
Junior Baez <junior_baez@ucne.edu.do>
2022-07-21 11:13:56 -0400

    Fix Typo

--- app/src/main/java/com/call/prestamoamigo/data/PersonasDao.kt
+++ app/src/main/java/com/call/prestamoamigo/data/PersonasDao.kt
@@ -35,12 +35,12 @@ interface PersonasDao {
 
     @Query(
         """
-        SELECT balance 
+        SELECT SUM(balance)
         FROM Prestamos
         WHERE personaId =:personaId
     """
     )
-    fun GetMontoFromPrestamos(personaId:Int): Double
+    fun GetMontoFromPrestamos(personaId:Int?): Double
 
     //SELECT fecha FROM Pagos WHERE
 }
