f89376ac10ca3a31236190f9854385003a76449b
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-07-22 15:00:04 -0400

    Mejorando diseño de registro

---
+++ app/src/main/java/com/call/prestamoamigo/view/Prestamo/TextObligatorio/TextObligatorio.kt
@@ -0,0 +1,27 @@
+package com.call.prestamoamigo.view.Prestamo.TextObligatorio
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TextObligatorio (error: Boolean){
+
+    val assistiveElementText = if(error) "Error: Obligatrio" else "*Obligatorio"
+    val assitiveElementColor = if(error){
+        MaterialTheme.colors.error
+    }else{
+        MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
+    }
+
+    Text(
+        text = assistiveElementText,
+        color = assitiveElementColor,
+        style = MaterialTheme.typography.caption,
+        modifier = Modifier.padding(start = 16.dp)
+    )
+}
