cd163cde60f0c8be58b8b59b1e2ac26c763c2353
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-07-25 13:36:30 -0400

    Arreglando diseño de consulta

--- app/src/main/java/com/call/prestamoamigo/view/Prestamo/ConsultaPrestamoScreen.kt
+++ app/src/main/java/com/call/prestamoamigo/view/Prestamo/ConsultaPrestamoScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -25,14 +26,22 @@ fun ConsultaPrestamoScreen(
     PrestamoViewModel: PrestamoViewModel = hiltViewModel(),
     personaIdentification: Int
 
-){
+) {
     val listaPrestamo = PrestamoViewModel.prestamos.collectAsState(initial = emptyList())
     val ScaffoldState = rememberScaffoldState()
 
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "Listado de Prestamos") })
+                title = { Text(text = "Listado de Prestamos") },
+                //actions = {
+                //   IconButton(onClick = {
+                //      navHostController.navigate("DashBoard")
+                //   }) {
+                //      Icon(Icons.Filled.ArrowBack, null)
+                //  }
+                // }
+            )
         },
         floatingActionButton = {
             FloatingActionButton(onClick = {
@@ -50,9 +59,13 @@ fun ConsultaPrestamoScreen(
             modifier = Modifier.padding(8.dp)
                 .fillMaxSize()
         ) {
-            LazyColumn(modifier = Modifier.fillMaxSize()){
-                items(listaPrestamo.value){ prestamo ->
-                    RowPrestamo(prestamo = prestamo, navHostController = navHostController, personaIdentification)
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(listaPrestamo.value) { prestamo ->
+                    RowPrestamo(
+                        prestamo = prestamo,
+                        navHostController = navHostController,
+                        personaIdentification
+                    )
                     Spacer(modifier = Modifier.height(5.dp))
                     Divider(
                         modifier = Modifier
@@ -65,4 +78,5 @@ fun ConsultaPrestamoScreen(
         }
     }
 }
+
 
