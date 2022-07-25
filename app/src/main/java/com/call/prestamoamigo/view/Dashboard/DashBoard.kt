b651fe8c5763516b48208f74b5b5bf4ea7c314f3
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2022-07-25 14:52:09 -0400

    Añadiendo funcionalidad al diseño del Dashboard

--- app/src/main/java/com/call/prestamoamigo/view/Dashboard/DashBoard.kt
+++ app/src/main/java/com/call/prestamoamigo/view/Dashboard/DashBoard.kt
@@ -1,15 +1,16 @@
 package com.call.prestamoamigo.view.Dashboard
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.SaveAs
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
@@ -33,13 +34,58 @@ Int, prestamosTotalesDelCliente: Int
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp)) {
-            Card (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(170.dp)
+            Card(modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp).padding(vertical = 5.dp)
+                , backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth().padding(5.dp),
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(color = Color(0xFF82D4BB))
+                            .padding(8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                    ) {
 
-                    ){
+                        Text(
+                            modifier = Modifier.padding(vertical = 5.dp),
+                            text = "$nombrePersona",
+                            style = MaterialTheme.typography.body1,
+                            fontFamily = FontFamily.Monospace,
+                            fontWeight = FontWeight.Bold,
+                        )
 
+                        Text(
+                            modifier = Modifier.padding(vertical = 5.dp),
+                            text = "$telefonoPersona",
+                            style = MaterialTheme.typography.body2,
+                            fontFamily = FontFamily.Monospace,
+                            fontWeight = FontWeight.Bold,
+                        )
+
+                        Text(
+                            modifier = Modifier.padding(vertical = 5.dp),
+                            text = "$correoPersona",
+                            style = MaterialTheme.typography.body1,
+                            fontFamily = FontFamily.Monospace,
+                            fontWeight = FontWeight.Bold,
+                        )
+
+                        Text(
+                            modifier = Modifier.padding(vertical = 5.dp),
+                            text = "$direccionPersona",
+                            style = MaterialTheme.typography.body2,
+                            fontFamily = FontFamily.Monospace,
+                            fontWeight = FontWeight.Bold,
+                        )
+
+                    }
+                }
             }
             Button(onClick = { navHostController.navigate("ConsultaPrestamo/$personaIdentification") },
                 modifier = Modifier
